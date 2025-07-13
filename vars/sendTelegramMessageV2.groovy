81521488c31500e008691fb98bf3fe32023f3ac0
KAY KEO <keokay888@gmail.com>
2025-07-13 16:39:50 +0700

    update telegram message

--- vars/sendTelegramMessageV2.groovy
+++ vars/sendTelegramMessageV2.groovy
@@ -1,9 +1,18 @@
-def call(String message, String token , String chatId) {
-    // uppgrade to use Markdown version instead 
-    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+def call(String message) {
+                
+    withCredentials([usernamePassword(credentialsId: 'TELEGRAM_BOT',
+    passwordVariable: 'TOKEN', usernameVariable: 'CHAT_ID')]) {
+      def encodedMessage = URLEncoder.encode(message, "UTF-8")
     sh """
-        curl -s -X POST https://api.telegram.org/bot${token}/sendMessage \\
-        -d chat_id=${chatId} \\
+        curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage \\
+        -d chat_id=${CHAT_ID} \\
         -d text="${encodedMessage}" > /dev/null
     """
+
+           
+        
+        
+    }     
+
+    
 }
