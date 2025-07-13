da2534d26412384bc1f7ac341465b633ce49cba7
KAY KEO <keokay888@gmail.com>
2025-07-13 16:46:20 +0700

    fix: docker push function

--- vars/pushDockerToDH.groovy
+++ vars/pushDockerToDH.groovy
@@ -2,7 +2,7 @@ def call(String imageName){
     withCredentials([usernamePassword(credentialsId: 'DOCKERHUB', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {    
         sh """
             echo '$PASSWORD' |  docker login -u $USERNAME --password-stdin   
-        docker push ${imageName}:${env.BUILD_NUMBER} 
+        docker push ${imageName}
         """
                     
     }
